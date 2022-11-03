acc6757104079377c3037e825a654a7367df4f39
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-03 15:39:16 +0800

    add @EnableOpenApi configuration

--- src/main/java/com/pttbackend/pttclone/PttcloneApplication.java
+++ src/main/java/com/pttbackend/pttclone/PttcloneApplication.java
@@ -3,6 +3,9 @@ package com.pttbackend.pttclone;
 import com.pttbackend.pttclone.config.OAuth2Properties;
 import com.pttbackend.pttclone.config.StorageProperties;
 
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+// import springfox.documentation.oas.annotations.EnableOpenApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +13,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 /**
@@ -22,6 +26,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * 		 org.springframework.boot.context.properties.EnableConfigurationProperties </a>
  * @see  org.springframework.scheduling.annotation.EnableAsync
  */
+@EnableOpenApi
 @SpringBootApplication
 @EnableConfigurationProperties({OAuth2Properties.class, 
 								StorageProperties.class})
