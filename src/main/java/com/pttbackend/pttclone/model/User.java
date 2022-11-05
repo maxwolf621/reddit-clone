ddc28f470d571308390a1df5b7f3ca7107d22237
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-05 23:40:29 +0800

    add bookmarks for posts and subs

--- src/main/java/com/pttbackend/pttclone/model/User.java
+++ src/main/java/com/pttbackend/pttclone/model/User.java
@@ -2,18 +2,20 @@ package com.pttbackend.pttclone.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static javax.persistence.GenerationType.IDENTITY;
-import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.LAZY;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
@@ -22,11 +24,8 @@ import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import io.swagger.annotations.ApiModel;
-
-
-
 import java.util.List;
+import java.util.Set;
 import java.time.Instant;
 
 /**
@@ -34,16 +33,14 @@ import java.time.Instant;
  * @see <a href="https://springbootdev.com/2018/03/13/spring-data-jpa-auditing-with-createdby-createddate-lastmodifiedby-and-lastmodifieddate/"> Annotation </a>
  * @see <a href="https://stackoverflow.com/questions/17445657/hibernate-onetomany-java-lang-stackoverflowerror"> JPA stackoverflow</a>
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@EqualsAndHashCode(exclude = {"favPosts", "favSubs" , "votes", "posts", "subs"} )
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
 @Entity
 @Table(name = "user", uniqueConstraints = {
     @UniqueConstraint(columnNames = "mail")
 })
-@ApiModel( value = "User Model", description = "To store the user information") 
+//@ApiModel( value = "User Model", description = "To store the user information") 
 public class User {
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -78,39 +75,59 @@ public class User {
     @Column(name = "about_me")
     private String aboutMe;
 
-    /**
-     * Bidirectional
-     * A user can create many subs
-     */
-    @OneToMany(fetch = LAZY,
-               cascade = ALL,
-               mappedBy = "user",
-               orphanRemoval = true)
+    // --- Associations 
+
+    @OneToMany(
+        fetch = LAZY,
+        cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.DETACH
+        },
+        mappedBy = "user",
+        orphanRemoval = true)
     private List<Sub> subs;
 
-    /**
-     * Bidirectional
-     * A user can post many posts
-     */
-    @OneToMany(fetch = LAZY,
-               cascade = ALL,
-               mappedBy = "user",
-               orphanRemoval = true)
+    @OneToMany(
+        fetch = LAZY,
+        cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.DETACH
+        },
+        mappedBy = "user",
+        orphanRemoval = true)
     private List<Post> posts; 
 
-    /**
-     * Bidirectional
-     * A user can have many votes 
-     * on many posts
-     */
-    @OneToMany(fetch = LAZY,
-              cascade = ALL,
-              mappedBy = "user")
+    /*
+    @OneToMany(
+        fetch = LAZY,
+        cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.DETACH
+        },
+        mappedBy = "user")
     private List<Vote> votes;
+    */
+    
+    @ManyToMany(
+        fetch = LAZY,
+        mappedBy = "users"
+    )
+    private Set<Post> favPosts; 
+
+    @ManyToMany(
+        fetch = LAZY,
+        mappedBy="asUserFavSub"
+    )
+    private Set<Sub> favSubs;
 
     @Override
     public String toString(){
         return this.username + this.mail + this.createdDate ;
     }
+
+
 }
 
