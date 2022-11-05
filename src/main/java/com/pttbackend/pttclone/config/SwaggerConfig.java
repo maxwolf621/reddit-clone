8096140388083bf940c17833063ee437b32bc9d8
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-05 23:36:25 +0800

    revise

--- src/main/java/com/pttbackend/pttclone/config/SwaggerConfig.java
+++ src/main/java/com/pttbackend/pttclone/config/SwaggerConfig.java
@@ -23,59 +23,41 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Value("${swaggerInfo.url")
     private String url;
 
-    /** 
-     * @see <a href="https://stackoverflow.com/questions/43545540/swagger-ui-no-mapping-found-for-http-request">
-     *　Details Mapping swagger-ui html　</a>
-     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
+        //Swagger UI property
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
-    /** 
-     * Swagger 2.x.x version 
-     *          
-                 
-    @Bean   
-    public Docket apiDocket(){
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo((ApiInfo) getApiInfo()).select()
-                 // INDICATE swagger only function api's url in this Control 
-                 // .apis(RequestHandlerSelector.basePackge("com.demo.scanOnlyThis_Controller"))
-                .apis(RequestHandlerSelectors.any()) // file(java packages) filter : any
-                .paths(PathSelectors.any()) // API's url path filter: any
-                .build();
-    }
-    */
-
-    /**
-     * @return Docket
-     */
     @Bean
     public Docket apiDocket(){
         // Swagger 3.0 : OAS_30
         return new Docket(DocumentationType.OAS_30)
-                    .apiInfo((ApiInfo) getApiInfo())
+                    .pathMapping("/")
+                    .enable(true)
+                    .apiInfo((ApiInfo) this.getApiInfo())
+                    .host("http://localhost:8080")
                     // to build apiSelector
                     .select()
+                    // scan the controller package
+                    .apis(RequestHandlerSelectors.basePackage("com.pttbackend.pttclone.controller"))
                     // files(java packages) filter : any
-                    .apis(RequestHandlerSelectors.any())
+                    // .apis(RequestHandlerSelectors.any())
                     // API's url path filter: any
                     .paths(PathSelectors.any()) 
                     .build();
     }
-
+    
     /**
      * @return Object {@code ApiInfo} 
      */
     private Object getApiInfo() {
         return new ApiInfoBuilder()
-                .title("My Spring Boot Project")
-                .version("2.0")
+                .title("Backend Project")
+                .version("3.0")
                 .description("Test")
                 //license("Apache License Version 2.0")
                 .contact(new Contact(name,url,mail)) 
