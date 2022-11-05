0bac3aa687051d6722dc1b91faa9491e8b907c29
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-05 23:41:17 +0800

    update subDTO and Sub Model

--- src/main/java/com/pttbackend/pttclone/mapper/SubMapper.java
+++ src/main/java/com/pttbackend/pttclone/mapper/SubMapper.java
@@ -2,19 +2,30 @@ package com.pttbackend.pttclone.mapper;
 
 import com.pttbackend.pttclone.dto.SubDTO;
 import com.pttbackend.pttclone.model.Sub;
+import com.pttbackend.pttclone.model.User;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring",  implementationPackage = "com.pttbackend.pttclone.mapper")
+@Mapper(componentModel = "spring")
 public interface SubMapper {
 
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "subname", source = "subname")
+    @Mapping(target = "description", source = "description")
     @Mapping(target = "postsCount" , expression = "java(getSize(sub))")
     SubDTO mapToSubDTO(Sub sub);
 
     default Integer getSize(Sub sub){
         return sub.getPosts().size();
     }
+
+    @Mapping(target = "subname", expression = "java(subDTO.getSubname())")
+    @Mapping(target = "description", expression = "java(subDTO.getDescription())")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "user", source = "user")
+    @Mapping(target = "asUserFavSub" , ignore = true)
+    Sub mapToSub(SubDTO subDTO, User user);
 }
 
   
