1536407a864170f5c73899e163e687fa0981e50a
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-05 23:39:50 +0800

    @Deprecated voteRepository

--- src/main/java/com/pttbackend/pttclone/repository/VoteRepository.java
+++ src/main/java/com/pttbackend/pttclone/repository/VoteRepository.java
@@ -3,6 +3,7 @@ package com.pttbackend.pttclone.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -21,5 +22,10 @@ public interface VoteRepository extends JpaRepository<Vote, Long> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Transactional
     @Query(value = "UPDATE Vote v SET v.vote_type =?1 where v.vote_id =?2", nativeQuery = true)
-    void setVoteTypeById(VoteType v, Long id);
+    void setVoteTypeById(Long v, Long id);
+
+    @Modifying
+    @Transactional
+    @Query("Delete From Vote v Where v.id = :id")
+    void deleteVoteById(@Param("id") Long id);
 }
