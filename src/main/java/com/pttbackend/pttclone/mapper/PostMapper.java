4aed0f077dba36fc3bd9c77e9036eaa9cb158c19
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-06 16:01:09 +0800

    using redis for post's votecount & down/up-vote

--- src/main/java/com/pttbackend/pttclone/mapper/PostMapper.java
+++ src/main/java/com/pttbackend/pttclone/mapper/PostMapper.java
@@ -7,13 +7,12 @@ import com.pttbackend.pttclone.model.Post;
 import com.pttbackend.pttclone.model.Sub;
 import com.pttbackend.pttclone.model.Tag;
 import com.pttbackend.pttclone.model.User;
-import com.pttbackend.pttclone.model.Vote;
+// import com.pttbackend.pttclone.model.Vote;
 import com.pttbackend.pttclone.model.VoteType;
 
 import static com.pttbackend.pttclone.model.VoteType.UPVOTE;
 import static com.pttbackend.pttclone.model.VoteType.DOWNVOTE;
 import com.pttbackend.pttclone.repository.CommentRepository;
-import com.pttbackend.pttclone.repository.FavoritePostRepository;
 import com.pttbackend.pttclone.repository.TagRepository;
 import com.pttbackend.pttclone.repository.VoteRepository;
 import com.pttbackend.pttclone.service.AuthenticationService;
@@ -21,18 +20,19 @@ import com.pttbackend.pttclone.service.AuthenticationService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 import static java.util.stream.Collectors.toSet;
 
-import java.util.Optional;
 import java.util.Set;
-
 
 /**
  * Map {@link PostRequest}, {@link Sub}, {@link User} to {@link Post} 
  * Map {@link Post} to {@link PostResponse} 
  */
-@Mapper(componentModel = "spring" ,  implementationPackage = "com.pttbackend.pttclone.mapper")
+@Mapper(componentModel = "spring"  )
 public abstract class PostMapper {
     
     @Autowired
@@ -43,12 +43,13 @@ public abstract class PostMapper {
     private AuthenticationService authService;
     @Autowired
     private TagRepository tagRepo;
+
     @Autowired
-    private FavoritePostRepository favoritePostRepo;
-
-
+    RedisTemplate<String, Integer> redisTemplate;
+    private HashOperations<String, String, Integer> hashOps; // downvote or upvote
+    private ValueOperations<String, Integer> valOps; // post count
+    
     /**
-     * 
      * Map {@link PostRequest}, {@link Sub}, {@link User} to {@link Post} 
      * @param postRequest {@link PostRequest}
      * @param sub {@link Sub}
@@ -76,6 +77,7 @@ public abstract class PostMapper {
     @Mapping(target ="username", source ="user.username")
     @Mapping(target = "commentCount", expression = "java(commentCount(post))")
     @Mapping(target = "duration", expression = "java(getDuration(post))")
+    @Mapping(target = "voteCount",expression = "java(getVoteCountFromRedisTemplate(post))")
     @Mapping(target = "upVote", expression = "java(isPostUpVoted(post))") // for user
     @Mapping(target = "downVote", expression = "java(isPostDownVoted(post))") // for user
     @Mapping(target = "tagnames", expression="java(tagsToStringList(post.getTags()))")
@@ -100,10 +102,18 @@ public abstract class PostMapper {
         return commentRepo.findByPost(post).size();
     }
 
+    Integer getVoteCountFromRedisTemplate(Post post){
+        valOps = redisTemplate.opsForValue();
+        String postId = String.valueOf(post.getId());
+        valOps.setIfAbsent(postId, 0);
+        return valOps.get(postId);
+    }
+
     // duration
     String getDuration(Post post) {
         return TimeAgo.using(post.getDuration().toEpochMilli());
     }
+
 
     boolean isPostUpVoted(Post post) {
         return checkVoteType(post, UPVOTE);
@@ -112,7 +122,19 @@ public abstract class PostMapper {
     boolean isPostDownVoted(Post post) {
         return checkVoteType(post, DOWNVOTE);
     }
+    
+    boolean checkVoteType(Post post, VoteType voteType){
+        hashOps = redisTemplate.opsForHash();
+        String postId = String.valueOf(post.getId());
+        User currentUser = authService.getCurrentUser();
+        String nameId = String.valueOf(currentUser.getUserId());
+        if (authService.isUserLoggedIn() && Boolean.TRUE.equals(hashOps.hasKey(nameId, postId))) {
+            return hashOps.get(nameId, postId) == voteType.getValue();
+        } 
+        return false; 
+    }
 
+    /* fetch vote via mysql 
     boolean checkVoteType(Post post, VoteType voteType) {
         if (authService.isUserLoggedIn()) {
             Optional<Vote> voteForPostByUser = voteRepo.findTopByPostAndUserOrderByVoteIdDesc(post,authService.getCurrentUser());
@@ -120,10 +142,12 @@ public abstract class PostMapper {
         }
         return false;
     }
+    */
+
 
     boolean postMarked(Post post){
         if( authService.isUserLoggedIn()){
-            return favoritePostRepo.findByUserAndFavPost(authService.getCurrentUser(), post).isPresent();
+            return authService.getCurrentUser().getFavPosts().contains(post);
         }
         return false;
     }
