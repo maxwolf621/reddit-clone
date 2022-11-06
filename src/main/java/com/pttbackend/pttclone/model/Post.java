541fd30e562f7e15b00537c576ad62ac335fa949
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2022-11-06 16:06:34 +0800

    add equalAndHashcode & manyTomany association for bookmark business logic layer

--- src/main/java/com/pttbackend/pttclone/model/Post.java
+++ src/main/java/com/pttbackend/pttclone/model/Post.java
@@ -5,14 +5,28 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Set;
 
+import org.hibernate.annotations.Cascade;
 import org.springframework.lang.Nullable;
 
 import static javax.persistence.FetchType.LAZY;
@@ -21,13 +35,10 @@ import static javax.persistence.FetchType.LAZY;
  * <p> Post's Entity </p>
  * <p> Do not using {@code @ToString} and {@code @Data} to avoid overflow error </p>
  */
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "post") 
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor
+@EqualsAndHashCode(exclude = {"comments", "tags", "users", "votes"})
+@Entity @Table(name = "post") 
 public class Post {
 
     // columns 
@@ -56,6 +67,13 @@ public class Post {
     @Column(name = "vote_count")
     private Integer voteCount;
 
+    @Override
+    public String toString(){
+        return "post id: " + id + "\n post name: " + postname + "\n vote count: " + voteCount;
+    }
+
+    /** ----   associations ----  */
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",  referencedColumnName = "user_id")
     private User user;
@@ -64,34 +82,51 @@ public class Post {
     @JoinColumn(name = "sub_id")
     private Sub sub; 
 
-    // ---------------------------
-
-    @OneToMany(fetch= LAZY,
-               mappedBy = "favPost",
-               cascade = {
-                CascadeType.MERGE, 
-                CascadeType.REFRESH, 
-                CascadeType.PERSIST 
-            },
-               orphanRemoval = true)
-    private List<FavoritePost> favoritePosts;
-
+    
+    /*    
     @OneToMany(fetch = LAZY,
                cascade = {
-                CascadeType.MERGE, 
-                CascadeType.REFRESH, 
-                CascadeType.PERSIST
-            },
+                   CascadeType.MERGE, 
+                   CascadeType.REFRESH, 
+                   CascadeType.PERSIST 
+               },
                mappedBy = "post",
                orphanRemoval = true)
-    private List<Vote> votes;
+    @Cascade({org.hibernate.annotations.CascadeType.SAVE_UPDATE,
+     org.hibernate.annotations.CascadeType.DELETE,
+     org.hibernate.annotations.CascadeType.MERGE,
+     org.hibernate.annotations.CascadeType.PERSIST})    
+    private Set<Vote> votes;
+    */
 
+    // bookmark
     @ManyToMany(
         fetch = LAZY,
         cascade = {
             CascadeType.PERSIST,
             CascadeType.MERGE,
             CascadeType.DETACH
+        }
+    )
+    @JoinTable(     
+        name = "post_user",
+        joinColumns = @JoinColumn(name = "post_id", updatable = true),
+        inverseJoinColumns = @JoinColumn(name = "user_id", updatable = true)
+    )
+    private Set<User> users;
+    public void addUser(User user){
+        users.add(user);
+        user.getFavPosts().add(this);
+    }
+    public void removeUser(User user){
+        users.remove(user);
+        user.getFavPosts().remove(this);
+    }
+
+    @ManyToMany(
+        fetch = LAZY,
+        cascade = {
+            CascadeType.ALL,
         }
     )
     @JoinTable(
@@ -110,5 +145,5 @@ public class Post {
         mappedBy = "post",
         orphanRemoval = true
     )
-    private Set<Comment> comments;
+    private Set<Comment> comments;   
 }
--- src/main/java/com/pttbackend/pttclone/model/Sub.java
+++ src/main/java/com/pttbackend/pttclone/model/Sub.java
@@ -1,13 +1,15 @@
 package com.pttbackend.pttclone.model;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.persistence.Column;
@@ -18,17 +20,16 @@ import javax.validation.constraints.NotBlank;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Entity
-@Table(name = "sub") 
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@EqualsAndHashCode(exclude = "posts")
+@Entity @Table(name = "sub") 
 public class Sub {
     @Id
     @GeneratedValue(strategy = SEQUENCE)
     @Column(name = "sub_id")
-    private Long Id;
+    private Long id;
     
     @NotBlank(message = "Community name is required")
     @Column(name = "name")
@@ -41,6 +42,12 @@ public class Sub {
     @Column(name = "createdDate")
     private Instant createdDate;
 
+    // ---- associations ---
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @OneToMany(fetch = FetchType.LAZY, 
                mappedBy ="sub",
                cascade = {
@@ -51,18 +58,27 @@ public class Sub {
                orphanRemoval = true)
     private List<Post> posts;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @OneToMany(fetch = FetchType.LAZY,
-               mappedBy = "favSub",
-               cascade = {
-                CascadeType.MERGE, 
-                CascadeType.REFRESH, 
-                CascadeType.PERSIST 
-            },
-               orphanRemoval = true)
-    private List<FavoriteSub> favoriteSubs; 
-    
+    @ManyToMany(
+        fetch = FetchType.LAZY,
+        cascade = {
+            CascadeType.MERGE, 
+            CascadeType.REFRESH, 
+            CascadeType.PERSIST 
+        }
+    )
+    @JoinTable(
+        name = "sub_user",
+        joinColumns = @JoinColumn(name="sub_id"),
+        inverseJoinColumns = @JoinColumn(name="user_id")
+    )
+    private Set<User> asUserFavSub = new HashSet<>();
+    public void addUser(User user){
+        asUserFavSub.add(user);
+        user.getFavSubs().add(this);
+    }
+    public boolean removeUser(User user){
+        boolean a = asUserFavSub.remove(user);
+        boolean b = user.getFavSubs().remove(this);
+        return a && b;
+    }
 }
